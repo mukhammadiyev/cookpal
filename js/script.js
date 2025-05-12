13d644017b13ff835c8cdf5ec46ab5917e155448
mukhammadiyev <muhammadsolihmuhammadiyev2009@gmail.com>
2025-05-12 16:38:09 +0500

    some bugs fixed

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// let modalBg = document.querySelector(".user-modal-bg")
+// let openCloseSet = document.querySelector("#open-close")
+
+// openCloseSet.addEventListener("click",()=>{
+// 	openCloseSet.classList.add("restarted")
+// })
