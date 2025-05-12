cd3bf5834f95a82740b2f4f805b342c30a508de2
mukhammadiyev <muhammadsolihmuhammadiyev2009@gmail.com>
2025-05-12 16:39:45 +0500

    cancelled

--- js/script.js
+++
@@ -1,6 +0,0 @@
-// let modalBg = document.querySelector(".user-modal-bg")
-// let openCloseSet = document.querySelector("#open-close")
-
-// openCloseSet.addEventListener("click",()=>{
-// 	openCloseSet.classList.add("restarted")
-// })
